b5d7b40309cf6f036f5a225d63ef215cb6386aa4
Richard Sartor <richard.sartor@rackspace.com>
2012-08-22 16:44:12 -0500

    Fixed for both new download and existing

--- get-ah.sh
+++ get-ah.sh
@@ -3,7 +3,8 @@
 if [ -e .git ]; then
   git pull origin master
 else
-  git clone https://github.com/rich4632/atom-hopper-setup.git .
+  git clone https://github.com/rich4632/atom-hopper-setup.git 
+  cd atom-hopper-setup
 fi
 
 chmod u+x *.sh
