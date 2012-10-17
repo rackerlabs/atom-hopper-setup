8c6f04c7e17089c99b2effa156aa9e5181fe99db
Richard Sartor <richard.sartor@rackspace.com>
2012-10-17 13:44:58 -0500

    Removed unneeded processing from get-ah.sh.

--- get-ah.sh
+++ get-ah.sh
@@ -7,25 +7,3 @@ else
   cd atom-hopper-setup
 fi
 
-chmod u+x *.sh
-chmod u+x setup*/*.sh
-
-if [ "$1" == "hostname" ]
-then
-  export REPLACE=$HOSTNAME
-elif [ "$1" == "no-domain" ]
-then
-  export REPLACE=
-else
-  # use the ip address
-  export REPLACE=`/sbin/ifconfig | grep '\<inet\>' | sed -n '1p' | tr -s ' ' | cut -d ' ' -f3 | cut -d ':' -f2`
-fi
-
-if [ "$REPLACE" != "" ]
-then
-  mv atom-server.cfg.xml atom-server.cfg.xml.2
-  cat atom-server.cfg.xml.2 | sed "s/\${hostname}/$REPLACE/g" > atom-server.cfg.xml
-  rm atom-server.cfg.xml.2
-fi
-
-
