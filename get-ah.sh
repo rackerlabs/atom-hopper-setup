17a9591b44da62a58e5fd0f95d752469064357c7
Richard Sartor <richard.sartor@rackspace.com>
2012-11-09 14:49:06 -0600

    Fixed the url of the repo.

--- get-ah.sh
+++ get-ah.sh
@@ -2,8 +2,16 @@
 
 if [ -e .git ]; then
   git pull origin master
+elif [ -e atom-hopper-setup ]; then
+  if [ -e atom-hopper-setup/.git ]; then
+    cd atom-hopper-setup
+    git pull origin master
+  else
+    echo "Can't clone the repo." >&2
+    exit 1
+  fi
 else
-  git clone https://github.com/rich4632/atom-hopper-setup.git 
+  git clone https://github.com/rackspace/atom-hopper-setup.git
   cd atom-hopper-setup
 fi
 
@@ -11,10 +19,8 @@ if [ "$1" == "checkout" ]
 then
   shift
   BRANCH=$1
+  git checkout $BRANCH
   shift
-else
-  BRANCH=master
 fi
 
-git checkout $BRANCH
 
